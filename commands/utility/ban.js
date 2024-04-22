bc7a44b5dc95a96c56b9f67dc4e56ba3a5116801
zhac164 <zhac164@live.rhul.ac.uk>
2024-04-22 22:46:41 +0100

    Extended ban command to include confirmation via buttons.

--- commands/utility/ban.js
+++ commands/utility/ban.js
@@ -1,12 +1,46 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+} = require("discord.js");
 
-const data = new SlashCommandBuilder()
-  .setName("ban")
-  .setDescription("Select a member and ban them.")
-  .addUserOption((option) =>
-    option
-      .setName("target")
-      .setDescription("The member to ban")
-      .setRequired(true)
-  )
-  .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers);
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("Select a member and ban them.")
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("The member to ban")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option.setName("reason").setDescription("The reason for the ban")
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+    .setDMPermission(false),
+  async execute(interaction) {
+    const target = interaction.options.getUser("target");
+    const reason = interaction.options.getString("reason");
+
+    const confirm = new ButtonBuilder()
+      .setCustomId("confirm")
+      .setLabel("Confirm Ban")
+      .setStyle(ButtonStyle.Danger);
+
+    const cancel = new ButtonBuilder()
+      .setCustomId("cancel")
+      .setLabel("Cancel")
+      .setStyle(ButtonStyle.Secondary);
+
+    const row = new ActionRowBuilder().addComponents(cancel, confirm);
+
+    await interaction.reply({
+      content: `Are you sure you want to bad ${target} for reason: ${reason}?`,
+      components: [row],
+      ephemeral: true,
+    });
+  },
+};
