a2b33ca7b2d973807290a3302744b99da97353e3
zhac164 <zhac164@live.rhul.ac.uk>
2024-04-23 15:13:48 +0100

    Refactored file structure to enable advanced command and event handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 
 // Create a new client instance
 const client = new Client({
@@ -14,34 +14,11 @@ const client = new Client({
 // event listener for event 'ready'
 client.on("ready", (c) => {
   console.log(`🐔 ${client.user.tag} is online.`);
-});
 
-client.on("interactionCreate", async (interaction) => {
-  try {
-    if (!interaction.isButton) return;
-
-    await interaction.deferReply({ ephemeral: true });
-
-    const role = interaction.guild.roles.cache.get(interaction.customId);
-    if (!role) {
-      interaction.editReply({
-        content: "I couldn't find that role.",
-      });
-      return;
-    }
-
-    const hasRole = interaction.member.roles.cache.has(role.id);
-    if (hasRole) {
-      await interaction.member.roles.remove(role);
-      await interaction.editReply(`The role ${role} has been removed.`);
-      return;
-    }
-
-    await interaction.member.roles.add(role);
-    await interaction.editReply(`The role ${role} has been added.`);
-  } catch (error) {
-    console.log(error);
-  }
+  client.user.setActivity({
+    name: "quak quak",
+    type: ActivityType.Custom,
+  });
 });
 
 client.login(process.env.TOKEN);
