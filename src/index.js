99f01aa81b90329a1c1cfb5ada9f7e16ea7e9ba7
zhac164 <zhac164@live.rhul.ac.uk>
2024-04-23 13:48:16 +0100

    Working 'hello' event listener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
 
+// Create a new client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+// event listener for event 'ready'
+client.on("ready", (c) => {
+  console.log(`🐔 ${client.user.tag} is online.`);
+});
+
+// event listener for event 'messageCreate'
+client.on("messageCreate", (message) => {
+  // exit method if the message author is a bot
+  if (message.author.bot) return;
+
+  // reply to a hello message
+  if (message.content === "hello") {
+    message.reply("Hey!");
+  }
+});
+
+client.login(process.env.TOKEN);
