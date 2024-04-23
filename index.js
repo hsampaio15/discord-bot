e297bf888e8eac668571cc43f94a92e5e3269dc4
zhac164 <zhac164@live.rhul.ac.uk>
2024-04-23 11:34:08 +0100

    Added empty music commands.

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
 
 client.cooldowns = new Collection();
 client.commands = new Collection();
